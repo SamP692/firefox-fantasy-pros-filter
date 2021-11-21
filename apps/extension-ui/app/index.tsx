2fca54ab2596aca38e5ebe4d5f6a6dc8c87a6873
Sam Peterson <samuel.e.peterson@gmail.com>
2021-11-20 19:46:04 -0600

    feat: create App component

---
+++ apps/extension-ui/app/index.tsx
@@ -0,0 +1,14 @@
+/* Libraries */
+import { h } from 'preact'
+
+/* Components */
+import { AppHeader } from '~ui/components'
+
+/* App */
+function App() {
+    return (
+        <AppHeader>Some App</AppHeader>
+    )
+}
+
+export default App
